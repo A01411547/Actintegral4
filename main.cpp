4e3abb2c7955b7e757e13f7e8f12a93cc9317081
A01411547 <57514414+A01411547@users.noreply.github.com>
2020-11-19 13:37:10 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-
+// Evidencia Grafos. Carolina Herrera Martinez A01411547
 
 #include <iostream>
 #include <cstring>
@@ -6,31 +6,59 @@
 
 using namespace std;
 
+void menuInicio(Graph g){
+    cout << "Introduce \"A\" para saber si todas las casas del vencindario son colindantes o no" << endl;
+    cout << "Introduce \"B\" para saber en que orden se encuentran las casas desde la entrada" << endl;
+    cout << "Introduce \"C\" para saber si las casas están en dos hileras o en una" << endl;        
+    char entrada;
+    cin >> entrada;
+  
+  if (entrada == 'A') {
+      cout<< ( true == (g.isTree() )? "No se ecuentran colindantes\n" : "Si se encuentran colindantes\n"   );
+    }
+    else if (entrada == 'B') {
+            cout << "El orden para recorrer las casas es " << g.topologicalSort() << endl;
+
+    }
+    else if (entrada == 'C') {
+            cout<< ( true == (g.bipartiteGraph() )? "No se ecuentran en dos hileras\n" : "Si se encuentran en dos hileras\n"   );
+
+     }
+
+  cout << endl << "Consulta terminada. Si deseas hacer otra consulta, introduce A, de lo contrario introduce B." << endl;
+  //char entrada;
+  cin>> entrada;
+  if (entrada == 'A'){
+    menuInicio(g);
+  }
+  else {
+    cout <<"Gracias por utilizar el sistema. Hasta pronto" << endl;
+  }
+
+}
+
+
 int main(int argc, char* argv[]) {
 
     cout << "Bienvenid@ al sistema de busqueda de casas. " << endl;
-    cout << "=======================================" << endl;
-// nota: poner aqui el atom
-
+    cout << "=======================================" << endl << endl;
+    
+    cout << "La base de datos \"datos.txt\" se encuentra siendo procesada" << endl; 
+    
     Graph g;
-    Graph star;
 
-    string g_ans, g_ans_2;
-    string star_ans, star_ans_2;
+    string g_ans;
 
     g.loadGraphList("datos.txt", 7, 7);
 
 
 
-    cout << " 1 " <<	(true == (g.isTree()) ? "success\n" : "fail\n");
+    cout << "Sistema listo." << endl << "A continuación, se muestra el menú de opciones."<<endl << endl;
 
+   menuInicio(g);
 
-    g_ans = "0 4 5 1 3 6 2";
-    cout << " 3 " <<	( (!g_ans.compare(g.topologicalSort()) || !g_ans_2.compare(g.topologicalSort())) ? "success\n" : "fail\n");
+    
 
-    star_ans = "0 1 2 3 4";
-
-    cout << " 5 " <<	(true == (g.bipartiteGraph()) ? "success\n" : "fail\n");
 
 
 }
